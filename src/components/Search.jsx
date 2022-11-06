22f981ff09a5811fcbeb05c38b98b2806d729ee3
suheyl-yazici <h.suheylyazici@gmail.com>
2022-11-06 21:49:34 +0300

    proje tamamlandı

---
+++ src/components/Search.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useGlobalContext } from "../context";
+
+
+const Search = () => {
+  const [text, setText] = useState("");
+  const { setSearchTerm , fetchRandomMeal } = useGlobalContext();
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(text){
+      setSearchTerm(text);
+    }
+  }
+
+  const handleRandomMeal = () =>{
+    setSearchTerm("");
+    setText("");
+    fetchRandomMeal();
+  }
+
+  return (
+    <header className="search-container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Type favorite meal" className="form-input" value={text} onChange={handleChange} />
+        <button type="submit" className="btn">Search</button>
+        <button type="button" className="btn btn-hipster" onClick={handleRandomMeal}>Suprise Me!</button>
+      </form>
+    </header>
+  )
+}
+
+export default Search
